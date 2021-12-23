23e2f2d73b47c5b7ca2fc700b3bdf1699ef41ea5
PSH101\terai <terai@pascal.jp>
2021-12-23 18:10:13 +0900

    ポートフォリオページ

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log("コンソール出力");
+//console.log("コンソール出力2");
+console.log("コンソール出力3");
+//関数の定義
+function greet(name) {
+	console.log(name + "さん、こんにちは");
+}
+
+//関数の呼び出し
+greet("山田");
+greet("鈴木");
+console.log(document.getElementById("menu"));
